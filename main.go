9df16076d1933a2abf242dc4173343a92a02c462
Gonzalo <kronleuchter85@gmail.com>
2024-01-12 15:29:05 +0000

    adding post method

--- main.go
+++ main.go
@@ -24,12 +24,21 @@ func getAlbums(c *gin.Context) {
 }
 
 func addAlbum(c * gin.Context ){
+	var newAlbum Album 
+	err := c.BindJSON(&newAlbum)
+
+	if err != nil {
+		return
+	}
+
+	albums = append(albums , newAlbum)
+	c.IndentedJSON(http.StatusCreated , newAlbum)
 }
 
 
 func main() {
     router := gin.Default()
     router.GET("/albums", getAlbums)
-
+	router.POST("/albums", addAlbum)
     router.Run("localhost:8080")
 }
